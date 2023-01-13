e2b3a6baed84e5b4a234074c99ee7e5aa4213b49
Budaházi Bence <budahazi.bence@petrik.hu>
2023-01-13 10:10:15 +0100

    Navigáció kotlin activityről

--- app/src/main/java/com/example/kotlinproject/KotlinActivity.kt
+++ app/src/main/java/com/example/kotlinproject/KotlinActivity.kt
@@ -1,11 +1,29 @@
 package com.example.kotlinproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class KotlinActivity : AppCompatActivity() {
+    private lateinit var visszaGomb: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+        init()
+        addListeners()
+    }
+
+    private fun addListeners() {
+        visszaGomb.setOnClickListener {
+            val intent = Intent(this@KotlinActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun init() {
+        visszaGomb = findViewById(R.id.visszaGomb)
     }
 }
